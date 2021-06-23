9797189cfe9489a1e39c77f480c877211a9b0089
Tanjim13 <tauhidtanjim13@gmail.com>
2021-06-23 10:55:25 +0600

    added 1D-Array.c

---
+++ 1D-Array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int A[1000],size,i;
+
+	printf("Size of integer datatype: %lu",sizeof(int));
+
+	printf("\nENTER THE Array SIZE\n");
+	scanf("%d",&size);
+	printf("\n Enter Array Elements:\n");
+
+	for(i=0;i<size;i++)
+	{
+		scanf("%d",&A[i]);
+	}
+	
+	printf("\nIndex   -   Address -  Value\n");
+	for (int i = 0; i < size; i++)
+	{
+		//printf("\n %d  - %u -  %d",i,&A[i],A[i]);
+
+	}
+
+	return 0;
+	
+}
